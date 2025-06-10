ac5528539c5014fa4589b481dba8e8aa485472ff
MBP27 <MBP27@mac.home>
2025-06-10 17:50:11 +0200

    add base working setup

---
+++ rive.js
@@ -0,0 +1,45 @@
+const stateMachine = "selector";
+
+const riveInstance = new rive.Rive({
+	src: "the_arcane.riv", //get rive file
+	canvas: document.getElementById("rive"), //get correct canvas
+	autoplay: true,
+	stateMachines: stateMachine, // get correct stateMachine
+	automaticallyHandleEvents: true, // Automatically handle RiveHTTPEvents
+	onLoad: onLoadHandler,
+	autoBind: true,
+});
+
+// Handle the onLoad event
+function onLoadHandler() {
+	// Prevent a blurry canvas by using the device pixel ratio
+	riveInstance.resizeDrawingSurfaceToCanvas();
+}
+
+// Resize the drawing surface if the window resizes
+window.addEventListener(
+	"resize",
+	() => {
+		riveInstance.resizeDrawingSurfaceToCanvas();
+	},
+	false
+);
+
+const eventFire = (riveEvent) => {
+	const eventData = riveEvent.data;
+	const eventName = eventData.name;
+	const eventProperties = eventData.properties;
+
+	const eventKey = eventName;
+	switch (eventKey) {
+		case "OnHoverEnter":
+			document.body.style.cursor = "pointer";
+			break;
+		case "OnHoverExit":
+			document.body.style.cursor = "auto";
+			break;
+		default:
+			console.log("Unhandled event:", eventName, "\n", riveEvent);
+			break;
+	}
+};
